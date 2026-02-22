f9ef8de75412660956a97476bdeccb41076c5f95
Damico707 <damian707rangel@gmail.com>
2026-02-22 16:00:04 -0500

    sepalabola

--- 20feb/script.js
+++ 20feb/script.js
@@ -8,23 +8,18 @@ const divs= document.querySelectorAll("div");
 console.log(divs);
 
 //cambiar el contendio del texto
-
-// const parrafo = document.getElementById('parrafoprueba')
-// parrafo.textContent="Parrafo Modificado"
-
+ const parrafo = document.getElementById('parrafoprueba')
+ parrafo.textContent="Parrafo Modificado"
 //cambiar todos los div por una etiqueta nueva
-
-// divs.forEach(div=>{
-//     const nuevoElemento=document.createElement("p")
-//     nuevoElemento.textContent="Nuevos elementos de parrafo"
-//     div.replaceWith(nuevoElemento)
-// })
-
-// const hipervinculo = document.querySelector(".card a")
-// const input= document.querySelector(".card input")
-
-// hipervinculo.setAttribute("href","https://www.google.com") //cambiar atributo
-// hipervinculo.textContent= "ir a pagina"
+ divs.forEach(div=>{
+     const nuevoElemento=document.createElement("p")
+     nuevoElemento.textContent="Nuevos elementos de parrafo"
+     div.replaceWith(nuevoElemento)
+ })
+ const hipervinculo = document.querySelector(".card a")
+ const input= document.querySelector(".card input")
+ hipervinculo.setAttribute("href","https://www.google.com") //cambiar atributo
+ hipervinculo.textContent= "ir a pagina"
 
 const boton= document.querySelector("button")
 
--- actividad/b.js
+++ actividad/b.js
@@ -1,1 +1,9 @@
 const enviar = document.querySelector("botoncito")
+    enviar.addEventListener("click")
+    let nombre =document.getElementById("name")
+    let edad = document.getElementById("edad")
+    let hobby = document.getElementById("hobby")
+    let texto = document.getElementById(p)
+    texto.textcontent = nombre , edad, hobby
+
+    
